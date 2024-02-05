38d27b5b3214a24b775a81e3e12e0ba7dce00481
GIMENAGASPAR <gimenagaspar7@gmail.com>
2024-02-05 12:58:24 -0300

    realizando conexion de servicio con api de prueba-instalacion de redux

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,20 @@
+import { createUser } from "../../redux/states/user";
+import { getMorty } from "../../services"
+import { useDispatch} from "react-redux";
 function Login() {
+  const dispatch = useDispatch();
+  const login = async()=>{
+    try{
+      const result = await getMorty();
+      dispatch(createUser(result));
+    }catch(error){}
+  };
+
   return (
-    <div>login</div>
+    <div>
+      <h2>LOGINNN</h2>
+      <button onClick={login}> LOGIN</button>
+    </div>
   )
 }
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { UserInfo } from "../models";
-import { userSlice } from "./states/user";
+import userSliceReducer from "./states/user";
+
 export interface AppStore{
     user: UserInfo;
 }
 
 export default configureStore <AppStore>({
     reducer:{
-        user: userSlice.reducer
+        user: userSliceReducer
     }
 })
